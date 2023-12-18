afe3417d4d5602aa4cdd1d2cea3de1552de46835
Humberto Pereira <humberto_pereira@live.com>
2023-12-18 01:45:09 +0100

    commit

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 // import ControlledFormHooks from "./components/ControlledFormHooks";
-import UseStateWithArrays from "./components/UseStateWithArrays";
+// import UseStateWithArrays from "./components/UseStateWithArrays";
+import UseStateWithObjects from "./components/UseStateWithObjects";
 
 function App() {
   return <div className="App">
-    <UseStateWithArrays />
+    <UseStateWithObjects />
   </div>;
 }
 
